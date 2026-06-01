7c9b58527fc44fb0effee88d5496658c8d690db3
Sai Leang Han <leang054944@gmail.com>
2026-06-01 14:25:00 +0530

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ DB_CONFIG = {
     "host": os.environ.get("MYSQL_HOST", "localhost"),
     "port": int(os.environ.get("MYSQL_PORT", 3306)),
     "user": os.environ.get("MYSQL_USER", "root"),
-    "password": os.environ.get("MYSQL_PASSWORD", "aiu@Leang7788"),
+    "password": os.environ.get("MYSQL_PASSWORD"),
     "database": os.environ.get("MYSQL_DATABASE", "school_erp"),
 }
 
